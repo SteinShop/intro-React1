77df73c3519f47b74433f395c7990b0eec4a51c1
SteinShop <shopstein85@gmail.com>
2025-05-26 22:18:36 -0300

    Criação site de dinossauro

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className='Body' >
+            <h2 className='titulo2' >Dinos são legais</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, suscipit cupiditate rerum ipsa tempora assumenda officia, odit velit laborum provident, nihil blanditiis corporis? Id accusantium a itaque autem assumenda reiciendis?</p>
+        </div>
+    )
+}
+
+export default Body
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ola/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='Header' >
+           
+                <h1>Dinossauros 😘😘👌</h1>
+           
+        </div>
+    )
+}
+
+export default Header
