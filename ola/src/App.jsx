e2b50bf62d87dfe6c1b400963e4e05c5f4e11d05
SteinShop <shopstein85@gmail.com>
2025-05-27 22:14:10 -0300

    Sim

--- ola/src/App.jsx
+++ ola/src/App.jsx
@@ -2,7 +2,11 @@ import './App.css'
 import Header from './components/Header'
 import Body from './components/Body'
 
+
 function App() {
+
+
+
  
 
   return (
--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,11 +1,81 @@
+import { useState } from 'react'
 import React from 'react'
 import './Body.css'
 
 function Body() {
+    const[cont, setCont] = useState(0)
+    const[inputValor, setInputValor] = useState('')
+    const[inputN1, setInputN1] = useState('')
+    const[inputN2, setInputN2] = useState('')
+
+    function aumentar(){
+        setCont(cont + 1)
+    }
+
+    function diminuir(){
+       if(cont > 0){
+        setCont(cont - 1)
+       }
+       else{
+        alert("Não é possivel ter números negativos")
+       }
+    }
+
+    function alterar (){
+        setCont(Number(inputValor))
+    }
+
+    function somar(){
+        setCont((Number(inputN1) + Number(inputN2)))
+
+    }
+
+    function subtrair(){
+        setCont((Number(inputN1) - Number(inputN2)))
+
+    }
+
+    function multiplicar(){
+        setCont((Number(inputN1) * Number(inputN2)))
+
+    }
+
+   
     return (
         <div className='Body' >
             <h2 className='titulo2' >Dinos são legais</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, suscipit cupiditate rerum ipsa tempora assumenda officia, odit velit laborum provident, nihil blanditiis corporis? Id accusantium a itaque autem assumenda reiciendis?</p>
+           <div className='cont' >
+           <button onClick={diminuir} >-</button>
+            <p>{cont}</p>
+            <button onClick={aumentar} >+</button>
+           </div>
+
+          <div className='form'>
+            <input type="number"
+             value={inputValor} 
+            onChange={(e) => setInputValor(e.target.value)}
+            />
+
+            <button onClick={alterar} >Alterar</button>
+          </div>
+
+          <div className='calculadera'>
+            <input type="text" 
+            value={inputN1}
+            onChange={(e) => setInputN1(e.target.value)}
+            />
+            <button onClick={somar} >+</button>
+            <button onClick={subtrair}>-</button>
+            <button onClick={multiplicar}>X</button>
+            <input type="text" 
+             value={inputN2}
+             onChange={(e) => setInputN2(e.target.value)}
+            /> 
+          </div>
+
+
+
+
         </div>
     )
 }
